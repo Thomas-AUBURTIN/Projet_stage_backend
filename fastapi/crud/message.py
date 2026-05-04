fd238a84a87c7d7b55225598c5058f571d7565ef
Thomas Auburtin <jeudego@hotmail.com>
2026-05-04 15:39:24 +0200

    acces_a_ollama_0.2.0

--- fastapi/crud/message.py
+++ fastapi/crud/message.py
@@ -2,7 +2,10 @@
 from models.message import Message
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
-from schemas.message import MessageCreation
+from schemas.message import MessageCreation,messageOllama
+import requests
+import json
+from datetime import datetime
 
 def getMessages(db : Session):
     try:
@@ -27,4 +30,34 @@ def createMessage(m : MessageCreation, db : Session):
     except SQLAlchemyError as e :
         db.rollback()
         raise
+
+def message_ollama(m : messageOllama, db : Session):
+    try:
+        data = {
+            "model" : "llama3:latest",
+            "messages" : [{"role":"user","content": m.question}],
+            "stream" : False
+        }
+        url ="http://localhost:11434/api/chat"
+        response = requests.post(url,json=data)
+        response_json = json.loads(response.text)
+        ai_reply = response_json["message"]["content"]
+        db_message = Message(
+                question = m.question,
+                reponse = ai_reply,
+                Datecreation=datetime.utcnow()
+                
+                )
+        db.add(db_message)
+        db.commit()
+        db.refresh(db_message)
+        return db_message
+    
+    except SQLAlchemyError as e :
+        db.rollback()
+        raise
+
+
+
+
 
--- fastapi/schemas/message.py
+++ fastapi/schemas/message.py
@@ -5,3 +5,10 @@ class MessageCreation(BaseModel):
     question : str = Field(min_length =1)
     reponse : str = Field(min_length =1)
     Datecreation : datetime = Field(default_factory=datetime.utcnow)
+
+
+
+class messageOllama(BaseModel):
+    question : str = Field(min_length =1)
+
+
